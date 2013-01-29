8108b78e54f6e1d20de25314cbf9b0bd8876a568
Marek J. Ryniejski <marekjulian@gmail.com>
2013-01-28 18:36:56 -0800

    Default config in ./config/default.js

---
+++ config/default.js
@@ -0,0 +1,58 @@
+//
+//  Default APP configuraiton. Composed of the following attributes:
+//    app: application configuration.
+//    db: database configuration.
+//    logging: logging configuraiton.
+//
+
+//
+//  appConfig:
+//
+var appConfig = {
+  appId: undefined,
+  appName: undefined
+};
+
+//
+//  dbConfig: Database configuration.
+//
+var dbConfig = { 
+  database: "plm-media-manager",
+  local: {
+    execPath: './MediaManagerTouchServer.app/Contents/MacOS/MediaManagerTouchServ',
+    port: "59840"
+  },
+  remote: {
+    host: "72.52.106.218",
+    port: undefined
+  }
+};
+
+//
+//  loggingConfig: Logging configuration
+//
+var loggingConfig = {
+  appenders: [
+    {
+      "type": "console",
+      "category": "console"
+    },
+    {
+      "type": "file",
+      "filename": "var/log/plm-media-manager.log",
+      "backups": 10,
+      "category": ["plm.MediaManagerApp", "plm.MediaManagerAppSupport", "plm.ImageService"]
+    }
+  ],
+  "levels": { "plm.MediaManagerApp" : "TRACE",
+              "plm.ImageService" : "ERROR" },
+  "replaceConsole": false
+};
+
+var config = {
+  app: appConfig,
+  db: dbConfig,
+  logging: loggingConfig
+};
+
+module.exports = config;
