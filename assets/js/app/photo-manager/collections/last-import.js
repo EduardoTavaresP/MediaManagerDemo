fca1e765f966102ed21e01a271895a5e6026bf3d
Marek Ryniejski <marekjulian@gmail.com>
2013-02-12 16:36:28 -0800

    update the last import

--- assets/js/app/photo-manager/collections/last-import.js
+++ assets/js/app/photo-manager/collections/last-import.js
@@ -5,23 +5,46 @@ define([
   // Grab the Importers collection as we need to get the last importer used to import images.
   'app/collections/importers',
   // Grab the Image model as last-import is just a collection of images associated with the most recent import.
+  'app/models/importer',
   'app/models/image'
 ],
-       function(_, Backbone, ImportersCollection, ImageModel) {
+       function(_, Backbone, ImportersCollection, ImporterModel, ImageModel) {
+
+         //
+         // LastImportCollection: collection of the images associated with the most recent import.
+         //
          var LastImportCollection = Backbone.Collection.extend({
 
            model: ImageModel,
 
+           //
+           //  The collection used to get to the importers.
+           //
            importers: undefined,
-           lastImporter: undefined,
+           //
+           //  For convenience, holds a reference to the importer associated with the most recent importer.
+           //
+           importer: undefined,
 
-           initialize: function() {
-             this.importers = new ImportersCollection(null, { numToImport: 1 } );
+           //
+           // initialize:
+           //
+           //  options:
+           //    importer - initialize with a specific instance of an importer.
+           //
+           initialize: function(options) {
+             if (options && _.has(options, 'importer')) {
+               this.importer = new ImporterModel(options.importer);
+               this.importers = new ImportersCollection([this.importer], { numToImport: 1 } );
+             }
+             else {
+               this.importers = new ImportersCollection(null, { numToImport: 1 } );
+             }
            },
 
            url: function() {
-             if (this.lastImporter) {
-               return '/api/media-manager/v0/importers/' + this.lastImporter.id + '/images';
+             if (this.importer) {
+               return '/api/media-manager/v0/importers/' + this.importer.id + '/images';
              }
              else {
                return undefined;
@@ -32,12 +55,15 @@ define([
              var that = this;
              var onImportersSuccess = function(importersCollection, response) {
                if (importersCollection.length > 0) {
-                 that.lastImporter = importersCollection.at(0);
-                 console.log('Last importer fetched, w/ id - ' + that.lastImporter.id);
+                 that.importer = importersCollection.at(0);
+                 console.log('Last importer fetched, w/ id - ' + that.importer.id);
                  Backbone.Collection.prototype.fetch.call(that, options);
                }
                else {
                  console.log('We have an empty collection of importers!');
+                 if (options && _.has(options, 'success')) {
+                   options.success(that, response, options);
+                 }
                }
              };
              var onImportersError = function(importersCollection, xhr) {
